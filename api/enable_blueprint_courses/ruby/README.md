0d47352181922b2d864b35c0ad2cc10239fd1af7
Chip Douglas <cbroadhead@instructure.com>
2019-03-14 10:21:23 -0600

    enable_blueprint_courses

---
+++ api/enable_blueprint_courses/ruby/README.md
@@ -0,0 +1,9 @@
+Enable Blueprint Courses
+---
+This script takes a csv file with one header **sis_course_id**. This column refers to the sis_id of the courses that should be enabled as a blueprint.
+
+In the script there is a variable **status** that needs to be changed. **Defaulted at *true***, but can be changed to *false* to turn off blueprint courses in bulk.
+
+**Best case use: Enabling blueprint courses in bulk.**
+
+Please see the `courses.csv` as a template to structure your CSV file.
---
+++ api/enable_blueprint_courses/ruby/enable_blueprint_courses.rb
@@ -0,0 +1,41 @@
+# Working as of 5/25/2018
+
+require 'csv'
+require 'typhoeus'
+require 'byebug'
+
+### CHANGE THESE VALUES
+domain = '' # e.g. 'domain' in 'https://domain.instructure.com'
+token = ''  # api token for account admin user
+env = ''	#Either blank for prod, or type test or beta
+status = 'true' # use 'false' to turn off blueprint courses
+csv = '' # Path to file that should contain a sis_course_id header
+
+#================
+# Don't edit from here down unless you know what you're doing.
+
+env != '' ? env << '.' : env
+base_url = "https://#{domain}.#{env}instructure.com/"
+test_url = "#{base_url}/accounts/self"
+
+raise "Error: can't locate the update CSV" unless File.exist?(csv)
+
+
+test = Typhoeus.get(test_url, followlocation: true)
+raise "Error: The token, domain, or env variables are not set correctly" unless test.code == 200
+
+CSV.foreach(csv, {:headers => true}) do |row|
+  enable_blueprint_course = Typhoeus.put(
+            base_url + "/api/v1/courses/sis_course_id:" + row['sis_course_id'] + "?course[blueprint]=#{status}",
+            headers: {:authorization => 'Bearer ' + token }
+            )
+  if enable_blueprint_course.code == 200
+    puts "Course #{row['sis_course_id']} blueprint course equals #{status}"
+  elsif enable_blueprint_course.code == 400
+    puts "Error: #{row['sis_course_id']} that didn't work Error 400."
+  else
+    puts "Course #{row['sis_course_id']} had failed blueprint course to be #{status}."
+    puts "Moving right along..."
+  end
+end
+puts "Finished enabling blueprint courses."
